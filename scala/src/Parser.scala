27b221a652baefe562fee9e04f294cfc4c9da305
Zak Cook <zak.cook@epfl.ch>
2017-10-11 14:29:36 +0200

    For mah baby

--- scala/src/Parser.scala
+++ scala/src/Parser.scala
@@ -1,19 +1,6 @@
 
 
 object Parser {
-  def parseCar(car: String): Car = {
-    val split = car.split(' ')
-    if (split.length != 3)
-      throw new IllegalArgumentException
-
-    val timestamp = split(0).toDouble
-    
-    val lane = parseDir(split(1))
-    val choice = parseDir(split(2))
-
-    new Car(timestamp, lane, choice)
-  }
-
   def parseDir(dir: String): Direction = dir match {
     case "N" => N
     case "S" => S
